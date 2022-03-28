93c206000619c93975c50b5c3720b106b99ea4e9
Sergey <fred17@mail.ru>
2022-03-28 16:13:28 +0300

    Added the registration and the login pages

--- celestial_annucator/views.py
+++ celestial_annucator/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
     context = {'title': 'FlyScanner - поиск авиабилетов'}
     return render(request, 'celestial_annucator/main.html', context)
+
+def login(request):
+    context = {'title': 'FlyScanner - Вход'}
+    return render(request, 'celestial_annucator/login.html', context)
+
+def registration(request):
+    context = {'title': 'FlyScanner - Вход'}
+    return render(request, 'celestial_annucator/registration.html', context)
