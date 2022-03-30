22cf7a3f8e56ca5633c36caafe1d92ef43ecaf02
TatianaKudryavtseva <75038895+TatianaKudryavtseva@users.noreply.github.com>
2022-03-30 14:50:25 +0300

    Add files via upload
    added menager.py and change models.py for authapp

--- authapp/models.py
+++ authapp/models.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from authapp.manager import UserManager
 
 
 # Create your models here.
 class User(AbstractUser):
+    username = None
+    email = models.EmailField('email address', unique=True, error_messages={
+            'unique': "A user with that email already exists."})
     age = models.PositiveIntegerField('Возраст', null=True)
     avatar = models.ImageField(upload_to='avatars', blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
