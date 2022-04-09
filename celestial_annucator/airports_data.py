f642e525e44a5f82b20a944809f8cd0dd58f72f0
Tetenkov Alexander <forthe@ya.ru>
2022-04-09 21:27:52 +0300

    правки CR

--- celestial_annucator/airports_data.py
+++ celestial_annucator/airports_data.py
@@ -1,5 +1,5 @@
 import requests
-import settings
+from celestial_annucator import settings
 
 airports_data = None
 
@@ -7,9 +7,11 @@ airports_data = None
 def load_airports_data():
     global airports_data
     resp = requests.get(settings.AIRPORTS_DATA_URL)
+    print(resp)
     if resp.status_code != 200:
         raise Exception('Failed to load airports data')
     airports_data = resp.json()
+    print(airports_data)
 
 
 def get_data():
