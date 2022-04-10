ae613770ee77047a29d0d415c3d728a6a9d0abb9
TatianaKudryavtseva <75038895+TatianaKudryavtseva@users.noreply.github.com>
2022-04-10 15:28:56 +0300

    Add files via upload
    add search_flight

--- celestial_annucator/views.py
+++ celestial_annucator/views.py
@@ -7,6 +7,11 @@ from common.travelpayouts_client import TravelPayoutsClient
 
 def index(request):
     context = {'title': 'FlyScanner - поиск авиабилетов'}
+    if request.META['QUERY_STRING']:
+        flights_data = search_flights(request)
+        print(flights_data)
+        return JsonResponse(flights_data, safe=False)
+
     return render(request, 'celestial_annucator/main.html', context)
 
 
@@ -19,4 +24,17 @@ def get_airports_by_term(request: wsgi.WSGIRequest):
     client = TravelPayoutsClient()
     term = request.GET['term']
     airports_data = client.get_airports_by_term(term)
+    print(airports_data)
     return JsonResponse(airports_data, safe=False)
+
+
+def search_flights(request):
+    params = {
+        'origin': request.GET['fromCity'],
+        'destination': request.GET['toCity'],
+        'departure_at': request.GET['dateStart'],
+        'return_at': request.GET['dateBack'],
+    }
+    client = TravelPayoutsClient()
+    data = client.prices_for_dates(**params)
+    return data
