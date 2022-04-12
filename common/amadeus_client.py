18aab0bd8bd7c98de7416b6b25a999fdb85b9ab3
Alexander Tetenkov <forthe@ya.ru>
2022-04-12 20:35:26 +0300

    remove tests

--- common/amadeus_client.py
+++ common/amadeus_client.py
@@ -10,18 +10,3 @@ class AmadeusClient(Client):
         super().__init__(client_id=settings.AMADEUS_API_KEY, client_secret=settings.AMADEUS_SECRET_KEY)
 
 
-if __name__ == '__main__':
-
-    client = Client(client_id='jtwPsO2MVYam4ph7QJ65GbK9jZQSerMe', client_secret='caYb6K7dRcZysXhe')
-    try:
-        response = client.shopping.flight_offers_search.get(
-            originLocationCode='MOW',
-            destinationLocationCode='MRV',
-            departureDate='2022-04-17',
-            adults=1,
-            currencyCode='RUB'
-        )
-        print(response.data)
-    except ResponseError as error:
-        print(error)
-
