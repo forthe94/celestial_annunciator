9faee2ce0214345452d589357897d99ccb60b284
Sergey <fred17@mail.ru>
2022-04-27 14:19:37 +0300

    Добавил в профиле логин только для чтения

--- authapp/forms.py
+++ authapp/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, \
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.EmailField(label="Адрес почты", widget=forms.TextInput(
+    username = forms.EmailField(label="Логин", widget=forms.TextInput(
         attrs={"placeholder": "example@example.ru"}))
     password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
 
@@ -23,7 +23,7 @@ class AuthUserCreationForm(UserCreationForm):
     password1 = forms.CharField(widget=forms.PasswordInput, label="Пароль")
     password2 = forms.CharField(widget=forms.PasswordInput,
                                 label="Подтвердите пароль")
-    email = forms.EmailField(label="Адрес почты", widget=forms.TextInput(
+    email = forms.EmailField(label="Логин", widget=forms.TextInput(
         attrs={"placeholder": "example@example.ru"}))
 
     class Meta:
@@ -38,17 +38,18 @@ class AuthUserCreationForm(UserCreationForm):
 
 
 class AuthUserChangeForm(UserChangeForm):
-    email = forms.EmailField(label="Адрес почты", widget=forms.TextInput(
-        attrs={"placeholder": "example@example.ru"}))
+    email = forms.EmailField(label="Логин", widget=forms.TextInput(
+        attrs={"readonly": "readonly"}))
     first_name = forms.CharField(label="Имя")
     last_name = forms.CharField(label="Фамилия")
 
     class Meta:
         model = get_user_model()
-        fields = ("first_name", "last_name", "email", "age", "avatar")
+        fields = ("email", "first_name", "last_name", "age", "avatar")
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             for field_name, field in self.fields.items():
                 field.widget.attrs['class'] = f'form-control {field_name}'
                 field.help_text = ''
+
